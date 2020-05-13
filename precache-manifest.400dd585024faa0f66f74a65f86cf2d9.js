cbb0f53188a1ad3b964269cc0a6c0356316ae8a3
mmollaahmadi <mollaahmadimohammad@gmail.com>
2020-05-13 15:25:28 +0430

    Updates

---
+++ precache-manifest.400dd585024faa0f66f74a65f86cf2d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9f50d2db1a86a03bceb277a492bab8",
+    "url": "/dicom-viewer/index.html"
+  },
+  {
+    "revision": "68b7eb5faa2adaaf7f49",
+    "url": "/dicom-viewer/static/css/main.fa5b3062.chunk.css"
+  },
+  {
+    "revision": "a0f5a84b58f813c6ad0c",
+    "url": "/dicom-viewer/static/js/2.ef0f63b9.chunk.js"
+  },
+  {
+    "revision": "68b7eb5faa2adaaf7f49",
+    "url": "/dicom-viewer/static/js/main.1f395c25.chunk.js"
+  },
+  {
+    "revision": "89d794ccdf36a8432fc0",
+    "url": "/dicom-viewer/static/js/runtime~main.9fe2b979.js"
+  }
+]);
